b54436aec38cd0de521210e92daf32132dce1f66
햄쥐 <97268540+Yejineeeee@users.noreply.github.com>
2026-02-06 01:14:15 +0900

    Add files via upload

---
+++ 5주차/bank.cpp
@@ -0,0 +1,52 @@
+﻿// bank.cpp : 이 파일에는 'main' 함수가 포함됩니다. 거기서 프로그램 실행이 시작되고 종료됩니다.
+//
+
+#include <iostream>
+using namespace std;
+
+class bank {
+    private:
+        int safe;   // 금고
+
+    public :
+        bank();
+        void use_counter(int _in, int _out);    // 입출금 창구 함수
+};
+
+bank::bank() {
+    safe = 1000;    // 은행 초기 금액 설정
+    cout << "최초 금고 : " << safe << endl;
+    cout << endl;
+}
+
+void bank::use_counter(int _in, int _out) {
+    safe += _in;    // 입금
+    safe -= _out;   // 출금
+
+    cout << "입금 : " << _in << endl;
+    cout << "출금 : " << _out << endl;
+    cout << "금고 : " << safe << endl;
+    cout << endl;
+}
+
+int main()
+{
+    bank my_bank;   // my_bank 인스턴스 생성
+
+    my_bank.use_counter(0, 20); // 출금 20
+    my_bank.use_counter(50, 0); // 입금 50
+    my_bank.use_counter(100, 50); // 입금 100, 출금 50
+
+    return 0;
+}
+
+// 프로그램 실행: <Ctrl+F5> 또는 [디버그] > [디버깅하지 않고 시작] 메뉴
+// 프로그램 디버그: <F5> 키 또는 [디버그] > [디버깅 시작] 메뉴
+
+// 시작을 위한 팁: 
+//   1. [솔루션 탐색기] 창을 사용하여 파일을 추가/관리합니다.
+//   2. [팀 탐색기] 창을 사용하여 소스 제어에 연결합니다.
+//   3. [출력] 창을 사용하여 빌드 출력 및 기타 메시지를 확인합니다.
+//   4. [오류 목록] 창을 사용하여 오류를 봅니다.
+//   5. [프로젝트] > [새 항목 추가]로 이동하여 새 코드 파일을 만들거나, [프로젝트] > [기존 항목 추가]로 이동하여 기존 코드 파일을 프로젝트에 추가합니다.
+//   6. 나중에 이 프로젝트를 다시 열려면 [파일] > [열기] > [프로젝트]로 이동하고 .sln 파일을 선택합니다.
